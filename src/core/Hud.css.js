4ca63d3b1217ba0c3192dad9cdcb2f0877e599e5
Noah <noahcoetsee@gmail.com>
2017-03-20 13:31:57 -0400

    Added reset properties

--- src/core/Hud.css.js
+++ src/core/Hud.css.js
@@ -2,3 +2,13 @@
  * @author TheCodeCrafter / http://HudJS.org/
  */
  
+var CSS = {
+ margin: 0,
+	padding: 0,
+	border: 0,
+	fontSize: 100,
+	font: 'inherit',
+	verticalAlign: 'baseline'
+};
+
+export { CSS };
